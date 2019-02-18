e100bc6f7f71611cca8d7c1ce06d3bb93d5628ca
Serafima Aseeva <julivel1607@gmail.com>
2019-02-18 10:39:15 +0300

    1st attempt

---
+++ 1.1 - factorial/ConsoleApp1/Program.fs
@@ -0,0 +1,12 @@
+﻿open System
+
+let rec accFact x acc =
+    if x=0 then acc
+    else accFact (x-1) (x*acc)
+let factorial n =
+    if n<0 then 
+       printfn "Wrong argument: n should be positive"
+       -1
+    else accFact n 1
+
+printfn "%i" (factorial(5))
