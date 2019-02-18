a37ae5afdbe254240fe8b1e71786c9a990aa4281
Serafima Aseeva <julivel1607@gmail.com>
2019-02-18 10:47:32 +0300

    1st attempt

---
+++ 1.4 - pow2/pow2/Program.fs
@@ -0,0 +1,32 @@
+﻿open System
+
+let rec binpow a n acc =
+   if (n=1) then a*acc
+   else if (n%2=1) then 
+      binpow
+         a
+         (n-1)
+         (acc*a)
+   else 
+      binpow
+         (a*a)
+         (n/2)
+         acc
+
+let rec fillList ls x n i =
+   if i=n then ls
+   else 
+      fillList
+         (x::ls)
+         (x/2)
+         n
+         (i+1)
+
+let f1 ls m n =
+   fillList
+      ls
+      (binpow 2 (m+n) 1)
+      (m+1)
+      0
+
+printfn "%A" (f1 [] 4 3)
