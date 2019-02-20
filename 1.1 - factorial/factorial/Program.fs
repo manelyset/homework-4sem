fbb2520430889cef979aec2ba5297aa95ceb44ca
Serafima Aseeva <julivel1607@gmail.com>
2019-02-20 19:44:52 +0300

    small changes, .suo deleted

---
+++ 1.1 - factorial/factorial/Program.fs
@@ -0,0 +1,12 @@
+﻿open System
+
+let rec accFactorial x acc =
+    if x = 0 then acc
+    else accFactorial (x - 1) (x * acc)
+let factorial n =
+    if n < 0 then 
+       printfn "Wrong argument: n should be positive"
+       -1
+    else accFactorial n 1
+
+printfn "%i" (factorial(5))
