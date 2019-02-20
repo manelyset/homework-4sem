ce6443adc444f43e77e107b1e8b4f7d4fd51a1d4
Serafima Aseeva <julivel1607@gmail.com>
2019-02-20 19:55:16 +0300

    .suo, bin. obj deleted; small changes

--- 1.2 - fibonacci/fibonacci/Program.fs
+++ 1.2 - fibonacci/fibonacci/Program.fs
@@ -1,13 +1,13 @@
 ﻿
-let rec accFibo n a b i =
-    if (i=n) then b
-    else accFibo n b (a+b) (i+1)
+let rec accFibonacci n previous1 previous2 i =
+    if (i = n) then previous2
+    else accFibonacci n previous2 (previous1 + previous2) (i + 1)
 let fibonacci n =
-   if (n<0) then
+   if (n < 0) then
       printfn "Wrong argument: n should be positive"
       -1
-   else if (n<2) then n
-   else accFibo n 0 1 1
+   else if (n < 2) then n
+   else accFibonacci n 0 1 1
 printfn "%i" (fibonacci(2))
 
 
