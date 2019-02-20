64df516e2316643e3cd5627041f1ca974986c141
Serafima Aseeva <julivel1607@gmail.com>
2019-02-20 20:38:15 +0300

    Update Program.fs

--- 1.1 - factorial/factorial/Program.fs
+++ 1.1 - factorial/factorial/Program.fs
@@ -1,12 +1,12 @@
-﻿open System
-
-let rec accFactorial x acc =
-    if x = 0 then acc
-    else accFactorial (x - 1) (x * acc)
+﻿
 let factorial n =
     if n < 0 then 
        printfn "Wrong argument: n should be positive"
        -1
-    else accFactorial n 1
+    else 
+       let rec accFactorial (x, acc) =
+          if x = 0 then acc
+          else accFactorial ((x - 1), (x * acc))
+       accFactorial (n,1)
 
 printfn "%i" (factorial(5))
