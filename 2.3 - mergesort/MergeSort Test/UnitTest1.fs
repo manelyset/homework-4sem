057cc58b5ff22e31ea3dd4fd5ade9a47631c5890
Serafima Aseeva <julivel1607@gmail.com>
2019-02-26 12:00:26 +0300

    delete palindrome and mergesort

--- 2.3 - mergesort/MergeSort Test/UnitTest1.fs
+++
@@ -1,9 +0,0 @@
-module Tests
-
-open NUnit.Framework
-open FsUnit
-open MergeSort
-
-[<Test>]
-let ``[1; 2; 3] should be splitted into [1] and [2; 3]`` = 
-    split [1; 2; 3] |> should equal ([1], [2; 3])
