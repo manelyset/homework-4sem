0fd0eecc7837e58e674339dd8fa5a4bc8bbe725f
Serafima Aseeva <julivel1607@gmail.com>
2019-03-14 21:27:21 +0300

    1st attempt

---
+++ 3.1-pairnumbers/PairNumbers/Program.fs
@@ -0,0 +1,23 @@
+﻿module PairNumbers
+
+/// <summary>Counts the pair numbers in the list using List.filter, List.map and List.fold .</summary>
+/// <param name="ls">The input list.</param>
+/// <returns>The number of pair numbers in the list.</returns>
+let pairNumbers1 ls =
+    ls |> List.filter (fun x -> x % 2 = 0) |> List.map (fun x -> 1) |> List.fold (+) 0
+
+/// <summary>Counts the pair numbers in the list using List.filter and List.fold .</summary>
+/// <param name="ls">The input list.</param>
+/// <returns>The number of pair numbers in the list.</returns>
+let pairNumbers2 ls =
+    ls |> List.filter (fun x -> x % 2 = 0) |> List.fold (fun acc x -> acc + 1) 0
+
+/// <summary>Counts the pair numbers in the list using Seq.choose and Seq.fold .</summary>
+/// <param name="ls">The input list.</param>
+/// <returns>The number of pair numbers in the list.</returns>
+let pairNumbers3 ls =
+    ls |> Seq.choose (fun x ->   
+                          match x with
+                              | x when x%2=0 -> Some(1)
+                              | _ -> None) 
+                              |> Seq.fold (+) 0 
---
+++ 3.1-pairnumbers/PairNumbersTest/UnitTest1.fs
@@ -0,0 +1,53 @@
+module Tests
+
+open NUnit.Framework
+open FsUnit
+open PairNumbers
+
+[<Test>]
+let ``f1: There are 5 pair numbers in [9; 28; 18; 26; 3; 26; 25; 10; 5; 15]`` () =
+    pairNumbers1 [9; 28; 18; 26; 3; 26; 25; 10; 5; 15] |> should equal 5
+
+[<Test>]
+let ``f1: There is no pair numbers in an empty list`` () =
+    pairNumbers1 [] |> should equal 0
+
+[<Test>]
+let ``f1: There are 4 pair numbers in [4; 40; 22; 16]`` () =
+    pairNumbers1 [4; 40; 22; 16] |> should equal 4
+
+[<Test>]
+let ``f1: There is no pair numbers in [7; 3; 11; 1]`` () =
+    pairNumbers1 [7; 3; 11; 1] |> should equal 0
+
+[<Test>]
+let ``f2: There are 5 pair numbers in [9; 28; 18; 26; 3; 26; 25; 10; 5; 15]`` () =
+    pairNumbers2 [9; 28; 18; 26; 3; 26; 25; 10; 5; 15] |> should equal 5
+
+[<Test>]
+let ``f2: There is no pair numbers in an empty list`` () =
+    pairNumbers2 [] |> should equal 0
+
+[<Test>]
+let ``f2: There are 4 pair numbers in [4; 40; 22; 16]`` () =
+    pairNumbers2 [4; 40; 22; 16] |> should equal 4
+
+[<Test>]
+let ``f2: There is no pair numbers in [7; 3; 11; 1]`` () =
+    pairNumbers2 [7; 3; 11; 1] |> should equal 0
+
+[<Test>]
+let ``f3: There are 5 pair numbers in [9; 28; 18; 26; 3; 26; 25; 10; 5; 15]`` () =
+    pairNumbers3 [9; 28; 18; 26; 3; 26; 25; 10; 5; 15] |> should equal 5
+
+[<Test>]
+let ``f3: There is no pair numbers in an empty list`` () =
+    pairNumbers3 [] |> should equal 0
+
+[<Test>]
+let ``f3: There are 4 pair numbers in [4; 40; 22; 16]`` () =
+    pairNumbers3 [4; 40; 22; 16] |> should equal 4
+
+[<Test>]
+let ``f3: There is no pair numbers in [7; 3; 11; 1]`` () =
+    pairNumbers3 [7; 3; 11; 1] |> should equal 0
