4dded0cc940123ed7c4326ec8f0e3ead9d31b0d4
Serafima Aseeva <julivel1607@gmail.com>
2019-03-21 09:51:35 +0300

    Update UnitTest1.fs

--- 3.2-mapTree/MapTreeTest/UnitTest1.fs
+++ 3.2-mapTree/MapTreeTest/UnitTest1.fs
@@ -4,6 +4,12 @@ open NUnit.Framework
 open FsUnit
 open MapTree
 
+let rec treesEqual t1 t2 =
+    match t1, t2 with
+    | Tip, Tip -> true
+    | Tree(x, t3, t4), Tree(y, t5, t6) when x = y -> (treesEqual t3 t5) & (treesEqual t4 t6)
+    | _, _ -> false
+
 [<Test>]
 let ``increment all elements``() =
     mapTree (fun x -> x + 1) (Tree(1, Tree(2, Tip, Tip), Tree(3, Tip, Tip))) |> 
@@ -11,8 +17,9 @@ let ``increment all elements``() =
 
 [<Test>]
 let ``square all elements``() =
-    mapTree (fun x -> x * x) (Tree(1.0, Tree(-2.0, Tree(4.5, Tip, Tip), Tip), Tree(-3.0, Tree(5.1, Tip, Tip), Tree(6.0, Tip, Tip)))) |> 
-    should equal (Tree(1.0,Tree(4.0,Tree(20.25,Tip,Tip),Tip), Tree(9.0,Tree(26.01,Tip,Tip),Tree(36.0,Tip,Tip))))
+    mapTree (fun x -> x * x) (Tree(1, Tree(-2, Tree(4, Tip, Tip), Tip), Tree(-3, Tree(5, Tip, Tip), Tree(6, Tip, Tip))))
+   |> should equal (Tree(1,Tree(4,Tree(16,Tip,Tip),Tip), Tree(9,Tree(25,Tip,Tip),Tree(36,Tip,Tip))))                
+  
 
 [<Test>]
 let ``convert to strings``() =
