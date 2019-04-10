cd1f801c26463166672c88f5f3af257e2927161c
Serafima Aseeva <47242035+manelyset@users.noreply.github.com>
2019-04-11 02:24:52 +0300

    Update Program.fs

--- 2.3-mergesort/MergeSort/Program.fs
+++ 2.3-mergesort/MergeSort/Program.fs
@@ -1,13 +1,13 @@
 ﻿module MergeSort
 
-let merge (list1, list2) = 
-    let rec mergeAcc (ls1, ls2) acc =
-       match (ls1, ls2) with
-       | (ls1, []) -> (List.rev ls1)@acc
-       | ([], ls2) -> (List.rev ls2)@acc
-       | (h1::t1, h2::t2) when h1 < h2 -> mergeAcc (ls1, t2) (h2::acc)
-       | (h1::t1, h2::t2) when h1 >= h2 -> mergeAcc (t1, ls2) (h1::acc)
-    mergeAcc (list1, list2) []
+let merge list1 list2 = 
+    let rec mergeAcc ls1 ls2 acc =
+        match ls1 ls2 with
+        | ls1, [] -> (List.rev ls1) @ acc
+        | [], ls2 -> (List.rev ls2) @ acc
+        | h1::t1, h2::t2 when h1 < h2 -> mergeAcc ls1 t2 (h2::acc)
+        | h1::t1, h2::t2 when h1 >= h2 -> mergeAcc t1 ls2 (h1::acc)
+    mergeAcc list1 list2 []
 
 let split ls =
     let splitSize = ((List.length ls) / 2)
@@ -22,5 +22,5 @@ let rec mergeSort ls =
         let (ls1, ls2) = split ls
         let sorted1 = mergeSort ls1
         let sorted2 = mergeSort ls2
-        merge (List.rev sorted1, List.rev sorted2)
+        merge List.rev sorted1 List.rev sorted2
        
