f3ec120e74bea0cd8ec44e28adb0fdf2e45a93ca
Serafima Aseeva <47242035+manelyset@users.noreply.github.com>
2019-04-11 02:18:39 +0300

    Update Program.fs

--- 2.2 - palindrome/Palindrome/Program.fs
+++ 2.2 - palindrome/Palindrome/Program.fs
@@ -12,8 +12,8 @@ let rec compareLists ls1 ls2 =
     | [], [] -> true
     | _, [] -> false
     | [], _ -> false
-    | h1::t1, h2::t2 when h1<>h2 -> false
-    | h1::t1, h2::t2 when h1=h2 -> compareLists t1 t2
+    | h1::t1, h2::t2 when h1 <> h2 -> false
+    | h1::t1, h2::t2 when h1 = h2 -> compareLists t1 t2
 
 let isPalindrome str =
     let strList = Seq.toList str
