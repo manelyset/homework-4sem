c2175d9b179a0ac47dedd98aa46c4739eb27d9f3
Serafima Aseeva <julivel1607@gmail.com>
2019-04-16 01:46:59 +0300

    almost finished + tests

--- 6-network/Network/Program.fs
+++ 6-network/Network/Program.fs
@@ -1,10 +1,8 @@
 ﻿module Network
 
-open FsRandom
+open System
 
-let state = createState xorshift (123456789u, 362436069u, 521288629u, 88675123u)
-
-type Computer (OSType:string, infected:bool) =
+type Computer (OSType:string, infected:bool, r:Random) =
     let mutable mInfected = infected
     member this.Infected
         with get () = mInfected
@@ -17,6 +15,34 @@ type Computer (OSType:string, infected:bool) =
         | _ -> 1.0
     member this.infect (otherComputer:Computer) =
         if infected then
-            otherComputer.Infected <- (Random.get ``[0, 1)`` state < otherComputer.infProbability)
+            otherComputer.Infected <- (r.NextDouble() < otherComputer.infProbability)
+  
+
+let infect (network:Array) (computersList:Array) =
+    for i in 0..(network.GetLength 1) do
+        for j in 0..(network.GetLength 2) do
+            if (network.[i].[j]) = 1 then
+                computersList.[i].infect(computersList.[j])
+
+let networkState (computersList:Array) =
+    Array.init (computersList.GetLength 1) (fun i -> computersList.[i].Infected)
             
+let printNetworkState (computersList:Array) =
+    for i in 0..(computersList.GetLength 1) do
+        match computersList.[i].Infected with
+        | true -> printfn ("Computer %i: infected") i
+        | false -> printfn ("Computer %i: not infected") i
+    
+let printStepByStep steps (network:Array) (computersList:Array)= 
+    let rec iterateStepByStep steps (network:Array) (computersList:Array) i =
+        match i with
+        | a when a = steps -> 
+            printfn "STEP %i" i
+            printNetworkState computersList
+        | _ ->
+            printfn "STEP %i" i
+            printNetworkState computersList
+            infect network computersList
+            iterateStepByStep steps network computersList (i+1)
+    iterateStepByStep steps network computersList 0
     
