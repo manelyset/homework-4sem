2e69874c1c202b72c1922b6dffa32fce6cdb0b58
Serafima Aseeva <julivel1607@gmail.com>
2019-04-18 11:56:47 +0300

    1st attempt

---
+++ 7.1-rounding/RoundingTests/UnitTest1.fs
@@ -0,0 +1,34 @@
+module Tests
+
+open NUnit.Framework
+open FsUnit
+open Rounding
+
+let rounding a = new RoundingBuilder(a)
+
+[<Test>]
+let ``simple calculations``() =
+    let res = rounding 3 {
+        let! a = 2.0 / 12.0
+        let! b = 3.5
+        return a / b
+    }
+    res |> should equal 0.048
+
+[<Test>]
+let ``calcultions with zero-rounding``() =
+    let res = rounding 0 {
+        let! a = 3.6 + 2.1
+        let! b = a / 4.0
+        return b + 1.3
+    }
+    res |> should equal 3.0
+
+[<Test>]
+let ``calculations with division by zero``() =
+    let res = rounding 2 {
+        let! a = 0.003
+        let! b = 7.1 / a
+        return b + 1.33
+    }
+    res |> should equal infinity
