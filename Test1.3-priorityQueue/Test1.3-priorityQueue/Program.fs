1bf432cd93cba36e0570ac3559784a4f1e6e2bc2
Serafima Aseeva <julivel1607@gmail.com>
2019-04-26 18:16:17 +0300

    +test for task 1.2

---
+++ Test1.3-priorityQueue/Test1.3-priorityQueue/Program.fs
@@ -0,0 +1,11 @@
+﻿open System.Collections
+
+// Learn more about F# at http://fsharp.org
+
+open System
+
+Type PQueue (queue:Array)=
+    member this.insert x priority =
+        queue.insert 0 (x, priority)
+
+    
