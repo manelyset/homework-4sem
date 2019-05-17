e3c1322842f2e2b6c9690611dd4bc52bc292103a
Serafima Aseeva <julivel1607@gmail.com>
2019-05-17 21:00:05 +0300

    task 2 without tests

--- Test1.2rewrite2/Test1.2rewrite2/Program.fs
+++ Test1.2rewrite2/Test1.2rewrite2/Program.fs
@@ -1,8 +1,25 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿module Rhomb
 
 open System
 
-[<EntryPoint>]
-let main argv =
-    printfn "Hello World from F#!"
-    0 // return an integer exit code
+let createLine nbStars length =
+    let putStar i nbStars1 length1 =
+        if (i > nbStars1 || i < (length1 - nbStars1) / 2) then ' '
+        else '*'
+    Seq.init length (fun i -> putStar i nbStars length)
+
+let createRhomb n = 
+    let rec createRhombAcc n1 stars i acc =
+        match i with
+        | n1 -> acc
+        | j when (j < (n1 / 2)) -> createRhombAcc n1 (stars + 2) (i + 1) ((createLine stars n1)::acc)
+        | j when (j >= (n1 / 2)) -> createRhombAcc n1 (stars - 2) (i + 1) ((createLine stars n1)::acc)
+    createRhombAcc (2 * n - 1) 1 0 []
+
+let printRhomb n =
+    let rec printList (ls : seq<char> list) =
+        match ls with
+        | [] -> printfn ("")
+        | h :: t -> printfn "%s" <| (string h)
+                    printList t
+    printList (createRhomb n)
