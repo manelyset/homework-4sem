95e0157d3ed3af60cd9adfbec6214456e9ab7841
Serafima Aseeva <julivel1607@gmail.com>
2019-05-28 15:01:44 +0300

    task 2

---
+++ test1.2-cars/test1.2-cars/Program.fs
@@ -0,0 +1,37 @@
+﻿module Auto
+
+type Auto (price:int, mark:string, forSale:bool) =
+    member this.Price = price
+    member this.Mark = mark
+    member this.ForSale = forSale
+
+type Car (price:int, mark:string, model:string, color:string, forSale:bool) =
+    inherit Auto(price, mark, forSale)
+    member this.Model = model
+    member this.Color = color
+
+type Truck (price:int, mark:string, weight:int, forSale:bool) =
+    inherit Auto(price, mark, forSale)
+    member this.Weight = weight
+
+/// <summary>
+/// Calculates the sum of auto prices for the autos which are for sale
+/// </summary>
+/// <param name="autos">The list of autos</param>
+/// <returns>The sum of prices</returns>
+let sumPrices (autos:(Auto list)) = 
+    List.filter (fun (a:Auto) -> a.ForSale) autos |> List.fold (fun acc (a:Auto) -> acc + a.Price) 0
+
+/// <summary>
+/// Creates a list of all marks of autos which are for sale
+/// </summary>
+/// <param name="autos">The list of autos</param>
+/// <returns>The list of marks without repetitions</returns>
+let marksList(autos:(Auto list)) =
+    let rec marksListAcc (autos:(Auto list)) (marks:Set<string>) =
+        match autos with
+        | [] -> marks
+        | h :: t -> marksListAcc t (Set.add h.Mark marks)
+    let set = marksListAcc (List.filter (fun (a:Auto) -> a.ForSale) autos) (Set.empty)
+    Set.fold (fun acc a -> a :: acc) [] set |> List.rev
+
