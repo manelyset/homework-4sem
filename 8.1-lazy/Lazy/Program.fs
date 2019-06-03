9744ee664d11627bb746c9e26af18271069dec4b
Serafima Aseeva <julivel1607@gmail.com>
2019-06-03 03:25:56 +0300

    single-threaded only

---
+++ 8.1-lazy/Lazy/Program.fs
@@ -0,0 +1,27 @@
+﻿module Lazy
+
+open System
+
+type ILazy<'a> =
+    abstract member Get: unit -> 'a
+
+type SingleThread<'a> (supplier : unit -> 'a) =
+    let mutable result = None
+    let mutable calculated = false
+    interface ILazy<'a> with
+        member this.Get () =
+            match result with
+            | Some x -> x
+            | None ->
+                calculated <- true
+                result <- Some(supplier())
+                supplier()
+
+type MultiThread<'a> (supplier : unit -> 'a) =
+
+type LockFree<'a> (supplier : unit -> 'a) =
+
+type LazyFactory<'a> (supplier : unit -> 'a) =
+    static member CreateSingleThreadedLazy supplier = new SingleThread<'a>(supplier)
+    static member CreateMultiThreadedLazy supplier = new MultiThread<'a>(supplier)
+    static member CreateLockFreeLazy supplier = new LockFree<'a>(supplier)
