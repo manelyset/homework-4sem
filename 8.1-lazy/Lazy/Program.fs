ad37190149d7ca3afa3e2e150b7df30613cddb08
Serafima Aseeva <julivel1607@gmail.com>
2019-06-03 03:37:50 +0300

    +multi-threaded

--- 8.1-lazy/Lazy/Program.fs
+++ 8.1-lazy/Lazy/Program.fs
@@ -1,23 +1,35 @@
 ﻿module Lazy
 
 open System
+open System.Threading
 
 type ILazy<'a> =
     abstract member Get: unit -> 'a
 
 type SingleThread<'a> (supplier : unit -> 'a) =
     let mutable result = None
-    let mutable calculated = false
     interface ILazy<'a> with
-        member this.Get () =
+        member this.Get() =
             match result with
             | Some x -> x
-            | None ->
-                calculated <- true
+            | None ->                
                 result <- Some(supplier())
                 supplier()
 
 type MultiThread<'a> (supplier : unit -> 'a) =
+    let mutable result = None
+    let lockobj = new Object()
+    interface ILazy<'a> with
+        member this.Get() =
+            Monitor.Enter lockobj
+            try
+                match result with
+                | Some x -> x
+                | None ->                
+                    result <- Some(supplier())
+                    supplier()
+            finally
+                Monitor.Exit lockobj
 
 type LockFree<'a> (supplier : unit -> 'a) =
 
