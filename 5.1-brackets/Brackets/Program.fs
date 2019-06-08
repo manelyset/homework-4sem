ad9e0606591b2d94790a41b519a665a0ea479ca3
Serafima Aseeva <julivel1607@gmail.com>
2019-06-08 15:18:51 +0300

    added brackets matcher

--- 5.1-brackets/Brackets/Program.fs
+++ 5.1-brackets/Brackets/Program.fs
@@ -4,6 +4,18 @@ type Stack<'a> =
    | Node of 'a * Stack<'a>
    | Null
 
+/// <summary>
+/// Matches the closing bracket with its pair
+/// </summary>
+/// <param name="bracket">The closing bracket: ), ] or }</param>
+/// <returns>The opening bracket for it</returns>
+let openingBracketFor bracket = 
+    match bracket with
+    | ')' -> '('
+    | ']' -> '['
+    | '}' -> '{'
+    | _ -> failwith "the parameter is not a closing bracket!"
+
 /// <summary>Tests whether the bracket sequence is balanced. 
 /// The brackets are of three types: (), {} or {}.</summary>
 /// <param name = "brackets"> A string to test.</param>
@@ -13,14 +25,8 @@ let checkSequence brackets =
         match brackets, stack with 
         | [], Null -> true
         | h :: t, _ when h = '(' || h = '{' || h = '[' -> stackCheck t (Node(h, stack))
-        | h :: t, Node (x, next) when h = ')' ->
-            if x <> '(' then false
-            else stackCheck t next
-        | h :: t, Node (x, next) when h = '}' ->
-            if x <> '{' then false
-            else stackCheck t next
-        | h :: t, Node (x, next) when h = ']' ->
-            if x <> '[' then false
+        | h :: t, Node (x, next) when h = ')' || h = ']' || h = '}' ->
+            if x <> (openingBracketFor h) then false
             else stackCheck t next
         | _, _ -> false
     stackCheck (Seq.toList brackets) Null
