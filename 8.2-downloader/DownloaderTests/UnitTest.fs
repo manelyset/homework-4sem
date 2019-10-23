58576307669e2aa270a91b2e537510216a785d5c
Serafima Aseeva <julivel1607@gmail.com>
2019-10-24 02:18:39 +0300

    isn't downloading from an existing url

--- 8.2-downloader/DownloaderTests/UnitTest.fs
+++ 8.2-downloader/DownloaderTests/UnitTest.fs
@@ -6,5 +6,11 @@ open Downloader
 
 [<Test>]
 let ``spbu.ru contains 217 links`` () =
-    let pages = pagesDownloader "spbu.ru"
-    pages.Length |> should equal 217
+    let pagelist = pagesDownloader "spbu.ru"
+    pagelist |> List.length |> should equal 217
+
+[<Test>]
+let ``trying to download from a nonexistent url`` () =
+    let pagelist = pagesDownloader "http://nonexistent111.ru/"
+    pagelist |> List.length |> should equal 1
+    pagelist |> List.head |> should equal None
