80417f2a8ef985a0c708d7f17ae0a8774a97cfbc
Serafima Aseeva <julivel1607@gmail.com>
2019-10-30 01:50:17 +0300

    added comments + tests

--- 8.1-lazy/Lazy/Program.fs
+++ 8.1-lazy/Lazy/Program.fs
@@ -3,12 +3,20 @@
 open System
 open System.Threading
 
+/// <summary>
+/// An interface for lazy calculator
+/// </summary>
 type ILazy<'a> =
     abstract member Get: unit -> 'a
 
+/// <summary>
+/// A single-threaded lasy calculator
+/// </summary>
 type SingleThread<'a> (supplier : unit -> 'a) =
     let mutable result = None
     interface ILazy<'a> with
+        /// <summary>Performs lazy calculations with one thread</summary>
+        /// <returns>A result of calculations of type 'a</returns>
         member this.Get() =
             match result with
             | Some x -> x
@@ -16,9 +24,15 @@ type SingleThread<'a> (supplier : unit -> 'a) =
                 result <- Some(supplier())
                 supplier()
 
+
+/// <summary>
+/// A multi-threaded lasy calculator
+/// </summary>
 type MultiThread<'a> (supplier : unit -> 'a) =
     let mutable result = None
     let lockobj = new Object()
+    /// <summary>Performs lazy calculations with multiple threads</summary>
+    /// <returns>A result of calculations of type 'a</returns>
     interface ILazy<'a> with
         member this.Get() =
             Monitor.Enter lockobj
@@ -31,9 +45,14 @@ type MultiThread<'a> (supplier : unit -> 'a) =
             finally
                 Monitor.Exit lockobj
 
+/// <summary>
+/// A multi-threaded lasy calculator with no lock guarantee
+/// </summary>
 type LockFree<'a> (supplier : unit -> 'a) =
     let mutable result = None
     let mutable called = false
+    /// <summary>Performs lazy calculations with multiple threads and without locking</summary>
+    /// <returns>A result of calculations of type 'a</returns>
     interface ILazy<'a> with
         member this.Get() = 
             if not called then
@@ -42,7 +61,10 @@ type LockFree<'a> (supplier : unit -> 'a) =
                 called <- true
             result.Value
 
+/// <summary>
+/// Creates calculators of one of three types
+/// </summary>
 type LazyFactory<'a> (supplier : unit -> 'a) =
-    static member CreateSingleThreadedLazy supplier = new SingleThread<'a>(supplier)
-    static member CreateMultiThreadedLazy supplier = new MultiThread<'a>(supplier)
-    static member CreateLockFreeLazy supplier = new LockFree<'a>(supplier)
+    static member CreateSingleThreadedLazy supplier = new SingleThread<'a>(supplier) :> ILazy<'a>
+    static member CreateMultiThreadedLazy supplier = new MultiThread<'a>(supplier) :> ILazy<'a>
+    static member CreateLockFreeLazy supplier = new LockFree<'a>(supplier) :> ILazy<'a>
--- 8.1-lazy/LazyTests/UnitTestLazy.fs
+++ 8.1-lazy/LazyTests/UnitTestLazy.fs
@@ -1,9 +1,30 @@
 module Tests
 
+open System.Threading
 open NUnit.Framework
 open FsUnit
 open Lazy
 
 [<Test>]
-let
+let ``simple tests for each type of lazy``() =
+    let lSingle = LazyFactory.CreateSingleThreadedLazy(fun () -> 10)
+    let lMulti = LazyFactory.CreateMultiThreadedLazy(fun () -> 11)
+    let lLF = LazyFactory.CreateLockFreeLazy(fun () -> 12)
+    lSingle.Get() |> should equal 10
+    lMulti.Get() |> should equal 11
+    lLF.Get() |> should equal 12
+
+[<Test>]
+let ``supplier should be called only once``() =
+    let mutable counterSingle = 0
+    let mutable counterMulti = 0
+    let mutable counterLF = 0
+    let lSingle = LazyFactory.CreateSingleThreadedLazy(fun () -> Interlocked.Increment(ref counterSingle))
+    let lMulti = LazyFactory.CreateMultiThreadedLazy(fun () -> Interlocked.Increment(ref counterMulti))
+    let lLF = LazyFactory.CreateLockFreeLazy(fun () -> Interlocked.Increment (ref counterLF))
+    for i in 1..1000 do
+        lSingle.Get()  |> should be (lessThanOrEqualTo 1)
+        lMulti.Get() |> should be (lessThanOrEqualTo 1)
+        lLF.Get() |> should be (lessThanOrEqualTo 1)
+
 
