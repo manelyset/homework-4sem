4da7b39e39a791d2266aa29ecc91b07a155886fb
Serafima Aseeva <julivel1607@gmail.com>
2019-11-05 02:10:12 +0300

    change names + add race tests

--- 8.1-lazy/LazyTests/UnitTestLazy.fs
+++ 8.1-lazy/LazyTests/UnitTestLazy.fs
@@ -7,24 +7,39 @@ open Lazy
 
 [<Test>]
 let ``simple tests for each type of lazy``() =
-    let lSingle = LazyFactory.CreateSingleThreadedLazy(fun () -> 10)
-    let lMulti = LazyFactory.CreateMultiThreadedLazy(fun () -> 11)
-    let lLF = LazyFactory.CreateLockFreeLazy(fun () -> 12)
-    lSingle.Get() |> should equal 10
-    lMulti.Get() |> should equal 11
-    lLF.Get() |> should equal 12
+    let STCalculator = LazyFactory.CreateSingleThreadedLazy(fun () -> 10)
+    let MTCalculator = LazyFactory.CreateMultiThreadedLazy(fun () -> 11)
+    let LFCalculator = LazyFactory.CreateLockFreeLazy(fun () -> 12)
+    STCalculator.Get() |> should equal 10
+    MTCalculator.Get() |> should equal 11
+    LFCalculator.Get() |> should equal 12
 
 [<Test>]
 let ``supplier should be called only once``() =
     let mutable counterSingle = 0
     let mutable counterMulti = 0
     let mutable counterLF = 0
-    let lSingle = LazyFactory.CreateSingleThreadedLazy(fun () -> Interlocked.Increment(ref counterSingle))
-    let lMulti = LazyFactory.CreateMultiThreadedLazy(fun () -> Interlocked.Increment(ref counterMulti))
-    let lLF = LazyFactory.CreateLockFreeLazy(fun () -> Interlocked.Increment (ref counterLF))
+    let STCalculator = LazyFactory.CreateSingleThreadedLazy(fun () -> Interlocked.Increment(ref counterSingle))
+    let MTCalculator = LazyFactory.CreateMultiThreadedLazy(fun () -> Interlocked.Increment(ref counterMulti))
+    let LFCalculator = LazyFactory.CreateLockFreeLazy(fun () -> Interlocked.Increment (ref counterLF))
     for i in 1..1000 do
-        lSingle.Get()  |> should be (lessThanOrEqualTo 1)
-        lMulti.Get() |> should be (lessThanOrEqualTo 1)
-        lLF.Get() |> should be (lessThanOrEqualTo 1)
+        STCalculator.Get()  |> should be (lessThanOrEqualTo 1)
+        MTCalculator.Get() |> should be (lessThanOrEqualTo 1)
+        LFCalculator.Get() |> should be (lessThanOrEqualTo 1)
+
+[<Test>]
+let ``check race for multi-threaded calculator``() =
+    let MTCalculator = LazyFactory.CreateMultiThreadedLazy(fun () -> new System.Object())
+    let sample = MTCalculator.Get ()
+    for i in 1..1000 do
+        ThreadPool.QueueUserWorkItem (fun obj -> (MTCalculator.Get ()).Equals sample |> should be True) |> ignore
+
+[<Test>]
+let ``check race for lock-free calculator``() =
+    let LFCalculator = LazyFactory.CreateLockFreeLazy(fun () -> new System.Object())
+    let sample = LFCalculator.Get ()
+    for i in 1..1000 do
+        ThreadPool.QueueUserWorkItem (fun obj -> (LFCalculator.Get ()).Equals sample |> should be True) |> ignore
+
 
 
